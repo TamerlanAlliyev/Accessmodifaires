fc1e631468059584e6bc78ae4b224ceca22fadd2
TamerlanAlliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2023-10-18 12:31:59 -0700

    Task 10.18.2023

---
+++ Shoop.App/Shoop.App/Program.cs
@@ -0,0 +1,64 @@
+﻿using Shoop.Core.Models;
+
+Product[] products = { };
+
+Console.WriteLine("1.Creat Product");
+Console.WriteLine("1.Show Products");
+Console.WriteLine("0.Close");
+string request = Console.ReadLine();
+
+while (request != "0")
+{
+    switch (request)
+    {
+        case "1":
+            CreateProduct();
+            break;
+        case "2":
+            ShowProducts();
+            break;
+        default:
+            Console.WriteLine("please add valid option");
+            break;
+    }
+    Console.WriteLine("1.Create student");
+    Console.WriteLine("2.Show students");
+    Console.WriteLine("0.close");
+    request = Console.ReadLine();
+
+
+    void CreateProduct()
+    {
+        Console.WriteLine("Name :");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Price :");
+        double Price = double.Parse(Console.ReadLine());
+        Console.WriteLine("Category :");
+        string Category = Console.ReadLine();
+        Console.WriteLine("StockCount :");
+        string StockCount = Console.ReadLine();
+        Array.Resize(ref products, products.Length + 1);
+        products[products.Length - 1] = products[0];
+        Console.WriteLine("Products created succesfuly");
+    }
+
+    void ShowProducts()
+    {
+        foreach (Product product in products)
+        {
+            Console.WriteLine(product.GetFullInfo());
+        }
+
+    }
+
+    string ValidateString(string data)
+    {
+        while (string.IsNullOrWhiteSpace(data))
+        {
+            Console.WriteLine("pleasa add valid value");
+            data = Console.ReadLine();
+        }
+        return data;
+    }
+
+}
---
+++ Shoop.App/Shoop.Core/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Shoop.Core.Models
+{
+
+    public class Product
+    {
+        public string Name;
+        public double Price;
+        public string Category;
+        public string StockCount;
+
+        public Product(string name, double price, string category, string stockcount)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            StockCount = stockcount;
+        }
+
+        public string GetFullInfo()
+        {
+            return $"Name : {Name} , Price {Price} , Category {Category} , Stock Count {StockCount}";
+        }
+
+    }
+
+}
